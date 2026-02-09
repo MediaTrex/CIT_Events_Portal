b7147632427ea374814ed7028badf2958fbaaea3
Roshan Patel <roshanpatel12309@gmail.com>
2026-02-09 23:50:32 +0530

    Changes mysql conn to pool

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -17,37 +17,13 @@ const pool = mysql.createPool({
 
 pool.getConnection()
   .then(conn => {
-    console.log("✅ MySQL Pool Connected");
+    console.log("--- MySQL Pool Connected ---");
     conn.release();
   })
   .catch(err => {
-    console.error("❌ MySQL Connection Error:", err.message);
+    console.error("--- MySQL Connection Error:", err.message);
   });
 
-export const getDB = async () => {
+export default async function getDB() {
   return pool.getConnection();
-};
-
-
-// import mysql from 'mysql2/promise';
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const createDB = async () => mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_NAME,
-//   multipleStatements : true
-// });
-
-// createDB().then(connection => {
-//   if (global.connection = connection) {
-//   console.log('--- Connected to MySQL ---');
-//   }else{
-//   console.log('--- MySQL Connection Error ---');
-//   }
-// });
-
-// export default createDB;
+}
