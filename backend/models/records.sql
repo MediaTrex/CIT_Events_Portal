7127dacb8f1da3f742d21d1d1a20d51ed54ec70c
Roshan Patel <roshanpatel12309@gmail.com>
2026-02-15 12:35:37 +0530

    Debug

--- backend/models/records.sql
+++ backend/models/records.sql
@@ -12,11 +12,30 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     college VARCHAR(255),
+    dob DATE,
+
+    user_type ENUM('CITian', 'External') NOT NULL,
+    external_types VARCHAR(255),
+
     role ENUM('admin', 'organizer', 'participant') NOT NULL,
     is_verified BOOLEAN DEFAULT FALSE,
     resetOtp VARCHAR(6),
     resetOtpExpiryAt TIMESTAMP,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CHECK (
+        (role = 'admin' AND dob IS NULL AND college IS NULL)
+        OR
+        (role = 'organizer' AND dob IS NULL AND college IS NULL)
+        OR
+        (role = 'participant' AND dob IS NOT NULL AND college IS NOT NULL)
+    )
+
+    CHECK (
+        (user_type = 'External' AND external_types IS NOT NULL)
+        OR
+        (user_type = 'CITian' AND external_types IS NULL)
+    )
 );
 
 -- events table
@@ -25,14 +44,21 @@ CREATE TABLE events (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     event_venue VARCHAR(255) NOT NULL,
-    entry_fee DECIMAL(10,2) NOT NULL,
+    event_type ENUM('paid', 'free') NOT NULL,
+    amount DECIMAL(10,2) DEFAULT 0,
     reg_start_date DATE NOT NULL,
     reg_deadline DATE NOT NULL,
     event_date DATE NOT NULL,
     event_time TIME NOT NULL,
     organizer_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (organizer_id) REFERENCES users(id) ON DELETE SET NULL
+    FOREIGN KEY (organizer_id) REFERENCES users(id) ON DELETE SET NULL,
+
+    CHECK (
+        (event_type = 'paid' AND amount > 0)
+        OR
+        (event_type = 'free' AND amount = 0)
+    )
 );
 
 -- team table 
@@ -79,13 +105,13 @@ CREATE TABLE event_registrations (
 );
 
 -- payments table
-CREATE TABLE payments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    registration_id INT NOT NULL,
-    amount DECIMAL(10,2) NOT NULL,
-    payment_method VARCHAR(50),
-    transaction_id VARCHAR(255),
-    status ENUM('initiated', 'success', 'failed') DEFAULT 'initiated',
-    paid_at TIMESTAMP,
-    FOREIGN KEY (registration_id) REFERENCES event_registrations(id) ON DELETE CASCADE
-);
+-- CREATE TABLE payments (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     registration_id INT NOT NULL,
+--     amount DECIMAL(10,2) NOT NULL,
+--     payment_method VARCHAR(50),
+--     transaction_id VARCHAR(255),
+--     status ENUM('initiated', 'success', 'failed') DEFAULT 'initiated',
+--     paid_at TIMESTAMP,
+--     FOREIGN KEY (registration_id) REFERENCES event_registrations(id) ON DELETE CASCADE
+-- );
