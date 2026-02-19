0b097e7d86d64b6385c0ace41cb081ae607eb919
Roshan Patel <roshanpatel12309@gmail.com>
2026-02-19 19:55:24 +0530

    Add roles to token

--- backend/lib/tokenUtils.js
+++ backend/lib/tokenUtils.js
@@ -1,14 +1,15 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (userId,res) => {
-  const token = jwt.sign({id:userId}, process.env.JWT_SECRET, {
+export const generateToken = ({userId,role}, res) => {
+  console.log(userId,role);
+  const token = jwt.sign({id:userId,role:role}, process.env.JWT_SECRET, {
     expiresIn: "7d",
   })
 
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
    
