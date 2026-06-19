8cc050a45f061579f64fda269f18b8f33ad7583d
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-19 15:57:07 +0545

    added home to routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
+import Home from "./pages/Home";
 
 function App() {
     return (
@@ -8,7 +9,7 @@ function App() {
                 path="/"
                 element={
                     <Layout>
-                        <h1>Home </h1>
+                        <Home />
                     </Layout>
                 }
             />
