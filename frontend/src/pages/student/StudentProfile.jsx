6210f29da4b186c3254e176e94053101ff7f7aa3
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-07-07 23:54:24 +0530

    add cursor-pointer class to buttons in StudentProfile component

--- frontend/src/pages/student/StudentProfile.jsx
+++ frontend/src/pages/student/StudentProfile.jsx
@@ -123,7 +123,7 @@ export default function StudentProfile() {
                             <button
                                 type="button"
                                 onClick={() => setIsEditing(true)}
-                                className= "cursor-pointer inline-flex items-center justify-center rounded-(--cit-radius-md) bg-amber-500 px-4 py-2 text-sm font-semibold text-white transition hover:bg-amber-600"
+                                className="cursor-pointer inline-flex items-center justify-center rounded-(--cit-radius-md) bg-amber-500 px-4 py-2 text-sm font-semibold text-white transition hover:bg-amber-600"
                             >
                                 Update Profile
                             </button>
@@ -171,7 +171,7 @@ export default function StudentProfile() {
                             <button
                                 type="button"
                                 onClick={() => setIsEditing((prev) => !prev)}
-                                className="inline-flex items-center justify-center rounded-(--cit-radius-md) border border-white/30 bg-white/15 px-4 py-2 text-sm font-semibold text-white transition hover:bg-white/25"
+                                className="cursor-pointer inline-flex items-center justify-center rounded-(--cit-radius-md) border border-white/30 bg-white/15 px-4 py-2 text-sm font-semibold text-white transition hover:bg-white/25"
                             >
                                 {isEditing ? "Cancel" : "Update Profile"}
                             </button>
