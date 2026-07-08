d895045feb3ae97940f466fb8137230aaff41e8d
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-07-08 16:39:38 +0530

    add SearchContextProvider to main.jsx for search functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, useLocation } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { SearchContextProvider } from "./context/SearchProvider.jsx";
 
 const queryClient = new QueryClient();
 
@@ -24,9 +25,11 @@ createRoot(document.getElementById("root")).render(
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
                 <ScrollToTop />
-                <AuthProvider>
-                    <App />
-                </AuthProvider>
+                <SearchContextProvider>
+                    <AuthProvider>
+                        <App />
+                    </AuthProvider>
+                </SearchContextProvider>
                 <Toaster
                     position="top-center"
                     toastOptions={{
